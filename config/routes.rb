d122b81c5aa6c8447a0d887ce940ec61de25cb1f
tterbow <b.wormsbecker@gmail.com>
2019-09-10 20:22:41 -0600

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  Rails.application.routes.draw do
+    root 'cars#index'
+    resources :cars
+  end
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
